2a333bed0bca2084e0c7006c54e7ccdfc6ccaf79
Luis rodas <luis.rodas@digitalstudio.com>
2018-12-27 14:46:53 -0800

    first test: run application from ADB shell tool

--- run_debugger.bat
+++ run_debugger.bat
@@ -3,3 +3,8 @@
 set CUR_DIR=%cd%
 rem Call setting config
 call "%CUR_DIR%\config.bat"
+
+rem Destroy previus session
+call "%ADB_PATH%\adb.exe" shell am force-stop %ANDROID_PACKAGE%
+rem Run session
+call "%ADB_PATH%\adb.exe" shell am start -n "%ANDROID_PACKAGE%/%MAIN_ACTIVITY%" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -D
