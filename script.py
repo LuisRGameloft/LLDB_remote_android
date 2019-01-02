de1744754a8387d55abf84f5be6c8846129b8b4f
Luis rodas <luis.rodas@digitalstudio.com>
2019-01-01 18:43:41 -0800

    get current PID and device's name to start lldb session

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ import subprocess
 import os.path
 import urllib
 import zipfile
+import time
+import signal
 
 g_adb_tool                  = os.environ['ADB_PATH'] + '/adb.exe'
 g_android_package           = os.environ['ANDROID_PACKAGE']
@@ -73,8 +75,29 @@ def main():
     #Install start_lldb_server.sh script into package folder /data/data/<package-id>/lldb/bin
     print "Debugger is running ..."
     command = g_adb_tool + " shell run-as " + g_android_package + " sh -c '/data/data/" + g_android_package + "/lldb/bin/start_lldb_server.sh /data/data/" + g_android_package + "/lldb unix-abstract /" + g_android_package + "-0 platform-1545976949340.sock \"lldb process:gdb-remote packets\"'"
-    subprocess.call(command)
+    debugger_process = subprocess.Popen(command, stdout=subprocess.PIPE)
     
+    # Get Current PID for current debugger session
+    command = g_adb_tool + " jdwp"
+    process_jdwp = subprocess.Popen(command, stdout=subprocess.PIPE)
+    time.sleep(0.5)
+    current_pid = process_jdwp.stdout.readline()
+    os.kill(process_jdwp.pid, signal.SIGTERM)
+
+    # Get Current Device's name connected
+    command = g_adb_tool + " devices"
+    process_device_name = subprocess.Popen(command, stdout=subprocess.PIPE)
+    #read dummy first line this is "List of devices attached"
+    process_device_name.stdout.readline()
+    device_name = process_device_name.stdout.readline().split()
+
+    
+    os.kill(debugger_process.pid, signal.SIGTERM)
+
+    print current_pid
+    print "[" + device_name[0] + "]"
+
+
 
 if __name__ == "__main__":
     main()
